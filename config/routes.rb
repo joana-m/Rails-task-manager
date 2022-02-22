93053d3cbf75bc3094635fcfe8ffdc699fa7450a
Joana Moufat <joana.moufat@gmail.com>
2022-02-22 15:18:34 +0800

    rails task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #READ
+  get '/tasks', to: "tasks#index", as: :task_index
+  get '/tasks/:id', to: "tasks#show", as: :task
+
+  #CREATE
+  get '/new', to: "tasks#new", as: :new_task
+  post '/tasks', to: "tasks#create"
+
+  #UPDATE
+  get '/tasks/:id/edit', to: "tasks#edit", as: :edit_task
+  patch 'tasks/:id', to: "tasks#update"
+
+  #DESTROY
+  delete 'task/:id', to: "tasks#destroy", as: :delete_task
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Task.create(title: Faker::Food.dish, details: Faker::Food.ingredient)
